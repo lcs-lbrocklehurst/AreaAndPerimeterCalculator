fa93f5d35356fe1b3148a3ad99e3bf8f35b74e2e
Lewis Brocklehurst <lbrocklehurst@lcs.on.ca>
2023-02-07 12:15:16 -0500

    Initial Commit

---
+++ AreaAndPerimeterCalculator/AreaAndPerimeterCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AreaAndPerimeterCalculatorApp.swift
+//  AreaAndPerimeterCalculator
+//
+//  Created by Lewis Brocklehurst on 2023-02-07.
+//
+
+import SwiftUI
+
+@main
+struct AreaAndPerimeterCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
